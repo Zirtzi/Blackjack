60df83274a5b1c8f0ebec99d6766a423650ddd01
Zirtzi <tjlarrechea@gmail.com>
2023-03-03 20:42:18 -0800

    3-3-23 Updates

---
+++ Python/Releases/Version 1.1/Blackjack.py
@@ -0,0 +1,28 @@
+from StructureV1_1 import *
+deck = Deck()
+deck.Shuffle()
+dealer_hand = Dealer_Hand()
+player_hand = Player_Hand()
+player_hand.Player_Name()
+Player_Bank = player_hand.Deposit()
+while len(deck.cards) >= 13 and Player_Bank > 0:
+    Player_Bank = play_hand(deck, dealer_hand, player_hand)[-1]
+    if len(deck.cards) >= 13 and Player_Bank > 0:
+        print("\n" f"Shuffling a new hand. \033[1;33;40m{player_hand.name}\033[0m bank:"
+              f" \033[1;31;40m{Player_Bank}\033[0m with \033[1;31;40m{len(deck.cards)}\033[0m cards remaining." "\n")
+        time.sleep(1)
+        continue
+    elif len(deck.cards) < 13 and Player_Bank > 0:
+        print("\n" f"Shuffling a new deck. \033[1;33;40m{player_hand.name}\033[0m bank:"
+              f" \033[1;31;40m{Player_Bank}\033[0m \033[1;31;40m{len(deck.cards)}\033[0m"
+              f" cards were left in the last deck." "\n")
+        deck = Deck()
+        deck.Shuffle()
+        time.sleep(1)
+        continue
+    elif Player_Bank <= 0:
+        print("\n" "You are out of money. You can no longer play." "\n")
+        time.sleep(1)
+        break
+    else:
+        continue
